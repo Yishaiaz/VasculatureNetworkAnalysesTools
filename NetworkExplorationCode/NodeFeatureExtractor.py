ac0a046dcbabc9cbfe20f1dbe016d73ef07976e3
wachtfl <leah.wach@gmail.com>
2023-01-03 14:10:33 +0200

    cleaning code

--- NetworkExplorationCode/NodeFeatureExtractor.py
+++ NetworkExplorationCode/NodeFeatureExtractor.py
@@ -1,21 +1,17 @@
-import matplotlib.pyplot as plt
 import graph_tool as gt
 import numpy as np
 from graph_tool import topology, draw
-import matplotlib.pyplot as plt
-import random
 from graph_tool import Graph, Vertex, EdgePropertyMap
-import pandas as pd
 from tqdm import tqdm
 
 
 def calc_3d_dist(c_1: list[float], c_2: list[float]) -> int:
-    '''
+    """
     calculate distnace between two 3-d points with x,y,z
     :param c_1: list of 3 floats that are x,y,z coordinates
     :param c_2: list of 3 floats that are x,y,z coordinates
     :return: float that is the distance
-    '''
+    """
     return np.sqrt((c_1[0] - c_2[0]) ** 2 + (c_1[1] - c_2[1]) ** 2 + (c_1[2] - c_2[2]) ** 2)
 
 
@@ -57,7 +53,6 @@ def graph_edge_direction(_g: Graph) -> list[float]:
 
 
 def ego_net_distance_N(_g: Graph, ego: Vertex, d: int) -> Graph:
-#    sd = gt.topology.shortest_path(_g, ego, weights=_g.edge_properties["length"], max_dist=d)
     sd = gt.topology.shortest_distance(_g, ego, weights=_g.edge_properties["length"], max_dist=d)
     ego_g = gt.GraphView(_g, vfilt=sd.a < _g.num_vertices())
     return ego_g
@@ -75,32 +70,32 @@ def draw_graph_coordinated(_g: Graph, output_path: str):
 
 
 def calculate_basic_graph_properties(_g: Graph):
-    '''
+    """
     return num edges, num of vertices, loops, num artery,
     :param _g:
     :return:
-    '''
+    """
     basic_feat = {"n_edges": _g.num_edges(), "n_vertices": _g.num_vertices()}
     return basic_feat
 
 
 def analyze_properties(_g: Graph, graph_name: str, component: str, properties_to_analyze: list[str]) \
         -> dict[str, float]:
-    '''
+    """
     function from @Yishaia
     :param component: 'v' if vertex property or 'e' if edge property
     :param _g: graph to extract features from
     :param properties_to_analyze: dictionary of key 'edge' or 'vertex' and value as list of property names
     :return: a dictionary with keys 'edge' and 'vertex'
-    '''
+    """
     properties_values_dict = {}
     for property_name in properties_to_analyze:
         property_values = _g.properties[(component, property_name)].fa  # filtered attributes only for subgraph
         if len(property_values) != 0:
             property_mean = float(np.mean(property_values))
             property_std = float(np.std(property_values))
-            properties_values_dict[graph_name + '_' + component + '_' + f'{property_name}'+'_mean'] = property_mean
-            properties_values_dict[graph_name + '_' + component + '_' + f'{property_name}'+'_std'] = property_std
+            properties_values_dict[graph_name + '_' + component + '_' + f'{property_name}' + '_mean'] = property_mean
+            properties_values_dict[graph_name + '_' + component + '_' + f'{property_name}' + '_std'] = property_std
     return properties_values_dict
 
 
@@ -108,6 +103,7 @@ def preprocess_graph(_g: Graph, inplace: bool = False) -> Graph:
     """
     MUST use on each network before all other analysis!
     removes duplicate edges and loops. Uses and returns a preprocessed copy of the graph.
+    :param inplace:
     :param _g:
     :return: Graph
     """
@@ -131,10 +127,16 @@ def preprocess_graph(_g: Graph, inplace: bool = False) -> Graph:
 
 
 def vertex_disconnected(_g: Graph, _v: int) -> bool:
-    '''
+    """
     checking if vertex v is connected to any other vertices in graph by an edge
     :param _g:  the graph
     :param _v: the vertex index in graph
     :return: bool, true if disconnected, false if connected
-    '''
+    """
     return _g.vertex(_v).in_degree() == 0 and _g.vertex(_v).out_degree() == 0
+
+
+def add_e_graph_float_property(_g: Graph, p_name: str, p_list: list[float]):
+    _g.edge_properties[p_name] = _g.new_edge_property("float")
+    for i, e in enumerate(_g.edges()):
+        _g.edge_properties[p_name][e] = p_list[i]
